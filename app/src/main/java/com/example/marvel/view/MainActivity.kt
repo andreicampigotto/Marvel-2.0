9fd0a7c5321818f22bee01a592eacd769985b007
Andrei Campigotto <andreicampigotto@gmail.com>
2022-09-02 19:42:38 -0300

    ajustado navigation

--- app/src/main/java/com/example/marvel/view/MainActivity.kt
+++ app/src/main/java/com/example/marvel/view/MainActivity.kt
@@ -26,9 +26,7 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
-            topLevelDestinationIds = setOf(),
-            fallbackOnNavigateUpListener = ::onSupportNavigateUp
-        )
+            navController.graph)
         binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
